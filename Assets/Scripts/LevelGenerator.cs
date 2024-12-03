ccd47ea99eb26f81ec38ee5d02522c7ab1636acf
mrblueegaming <107106283+mrblueegaming@users.noreply.github.com>
2024-12-03 13:52:09 +0100

    Inital Commit
    inital commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject platformPrefab;
+    public GameObject springPrefab;
+    public GameObject superSpringPrefab;
+    public GameObject slowerPrefab;
+
+    public int NumberOfPlatforms;
+    public int NumberOfSprings;
+    public int NumberOfSuperSprings;
+    public int NumberOfSlower;
+
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i = 0; i < NumberOfPlatforms; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(Randomizer(), spawnPosition, Quaternion.identity);
+        }
+    }
+
+    GameObject Randomizer()
+    {
+        int r = Random.Range(0, 100);        
+        if (r > 1 && r < 3)
+        {
+            Debug.Log(r);
+            if (NumberOfSuperSprings > 0)
+            {                
+                NumberOfSuperSprings--;
+                return superSpringPrefab;
+            }
+            else
+            {
+                return platformPrefab;
+            }            
+        }
+        else if (r > 10 && r < 15)
+        {
+            Debug.Log(r);
+            if (NumberOfSprings > 0)
+            {
+                NumberOfSprings--;
+                return springPrefab;
+            }
+            else
+            {
+                return platformPrefab;
+            }
+        }
+        else if (r > 56 && r < 61)
+        {
+            Debug.Log(r);
+            if (NumberOfSlower > 0)
+            {
+                NumberOfSlower--;
+                return slowerPrefab;
+            }
+            else
+            {
+                return platformPrefab;
+            }
+        }
+        else
+        {
+            Debug.Log(r);
+            return platformPrefab;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float movement = 0f;
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * speed;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+}
